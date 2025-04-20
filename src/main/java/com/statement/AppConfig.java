a4d1d856df03010075adb3439cdddc2a2de74952
Hari <57590886+harikumar-star@users.noreply.github.com>
2025-04-20 13:05:43 +0530

    Update AppConfig.java
    thi

--- src/main/java/com/statement/AppConfig.java
+++ src/main/java/com/statement/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     //first setup
     // i done the second changes
-    // third
+    // thirds
     @Bean
     public BlockingQueue<byte[]> queue() {
         return new LinkedBlockingQueue<>();
